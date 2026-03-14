1dd03227020db31051e40e05708ed5fe46082474
abolambA <mn121529@gmail.com>
2026-03-15 00:04:55 +0400

    fix: flutter clean + final stat_card and main_shell corrections

--- smartdesk_companion/test/widget_test.dart
+++ smartdesk_companion/test/widget_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smartdesk_companion/main.dart';
 
 void main() {
-  testWidgets('SmartDesk app smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SmartDeskApp());
+  testWidgets('App launches without error', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
+    await tester.pumpWidget(SmartDeskApp(prefs: prefs));
     expect(find.byType(SmartDeskApp), findsOneWidget);
   });
 }
